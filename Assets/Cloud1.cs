3149aff64bec460ed3cbbd7704c3d86c0333914a
GeneralSkeltal <cjv81@scarletmail.rutgers.edu>
2022-10-02 06:25:07 -0400

    I made a house and don't know how to look at it from the back because updated unity is fucking stupid with outdated instructions

--- Assets/Cloud1.cs
+++ Assets/Cloud1.cs
@@ -49,10 +49,10 @@ public class Cloud1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Restart();
-        }
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+            //Restart();
+        //}
     }
     void Restart()
     {
